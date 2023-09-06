e229a871da92d98e577dc12882ad4f798d1948c7
Prajwal Kene <prajwalnk123@gmail.com>
2023-09-06 14:48:31 +0530

    parsing an object in useState

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,20 +3,25 @@ import { useState } from "react";
 //Blogging App using Hooks
 export default function Blog() {
   //creting a state for title
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-//   empty array to store the title and content of the previoes blogs
+  // const [title, setTitle] = useState("");
+  // const [content, setContent] = useState("");
+
+  const [formData, setFormData] = useState({title:"", content:""})
+  //   empty array to store the title and content of the previoes blogs
   const [blogs, setBlogs] = useState([]);
 
-
-//Passing the synthetic event as argument to stop refreshing the page on submit
+  //Passing the synthetic event as argument to stop refreshing the page on submit
   function handleSubmit(e) {
     e.preventDefault();
 
-    //using REST OPERATOR in javascript in which 
+    //using REST OPERATOR in javascript in which
     // array is holding the object of title and content of blog
-    setBlogs([{title, content},...blogs]);
-    console.log(blogs)
+    setBlogs([{ title:formData.title, content:formData.content }, ...blogs]);
+    //making the field titile and content empty after
+    //  printing the blogs titile and content
+    setFormData({title: formData.title, content:formData.content})
+    console.log(blogs);
+    
   }
 
   return (
@@ -27,15 +32,16 @@ export default function Blog() {
       {/* Division created to provide styling of section to the form */}
       <div className="section">
         <form onSubmit={handleSubmit}>
-        {/* Form for to write the blog */}
+          {/* Form for to write the blog */}
           {/* Row component to create a row for first input field */}
           <Row label="Title">
             <input
               className="input"
               placeholder="Enter the Title..."
-              value={title}
-            //   setting the title
-              onChange={(e)=>setTitle(e.target.value)}
+              value={formData.title}
+              //   setting the title
+              // setFormData accepting the object
+              onChange={(e) => setFormData({title:e.target.value, content:formData.content})}
             />
           </Row>
 
@@ -44,8 +50,8 @@ export default function Blog() {
             <textarea
               className="input content"
               placeholder="Content of the Blog..."
-              value={content}
-              onChange={(e)=>setContent(e.target.value)}
+              value={formData.content}
+              onChange={(e) => setFormData({title:formData.title, content:e.target.value})}
             />
           </Row>
 
@@ -59,13 +65,12 @@ export default function Blog() {
       {/* Section where submitted blogs will be displayed */}
       <h2> Blogs </h2>
       {/* mapping our the previous blogs and showing it on the screen */}
-      {blogs.map((blog,i) => (
+      {blogs.map((blog, i) => (
         <div className="blog" key={i}>
-            <h3>{blog.title}</h3>
-            <h3>{blog.content}</h3>
+          <h3>{blog.title}</h3>
+          <p>{blog.content}</p>
         </div>
       ))}
-      
     </>
   );
 }
