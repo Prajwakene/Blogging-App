9a4b7d85dbd80d2fab31460b9bcee8b3c09cd121
Prajwal Kene <prajwalnk123@gmail.com>
2023-09-07 10:50:50 +0530

    focus on the input field

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+//using useRef hook for focus the input filed
+import { useState, useRef } from "react";
 
 //Blogging App using Hooks
 export default function Blog() {
@@ -6,9 +7,12 @@ export default function Blog() {
   // const [title, setTitle] = useState("");
   // const [content, setContent] = useState("");
 
-  const [formData, setFormData] = useState({title:"", content:""})
+  const [formData, setFormData] = useState({ title: "", content: "" });
   //   empty array to store the title and content of the previoes blogs
   const [blogs, setBlogs] = useState([]);
+  //nulll for the initial render ...anyway its going to change after the initail render
+  //this titleRef should be assign to field i want to reference to
+  const titleRef = useRef(null);
 
   //Passing the synthetic event as argument to stop refreshing the page on submit
   function handleSubmit(e) {
@@ -16,16 +20,18 @@ export default function Blog() {
 
     //using REST OPERATOR in javascript in which
     // array is holding the object of title and content of blog
-    setBlogs([{ title:formData.title, content:formData.content }, ...blogs]);
+    setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
     //making the field titile and content empty after
     //  printing the blogs titile and content
-    setFormData({title:"" , content:""})
+    setFormData({ title: "", content: "" });
+    //to focus the input field
+    titleRef.current.focus();
     console.log(blogs);
-    
   }
 
-  function removeBlog(i){
-    setBlogs(blogs.filter((blog,index) => i!==index));
+  
+  function removeBlog(i) {
+    setBlogs(blogs.filter((blog, index) => i !== index));
   }
   return (
     <>
@@ -40,11 +46,18 @@ export default function Blog() {
           <Row label="Title">
             <input
               className="input"
-              placeholder="Enter the Title..."
+              placeholder="Enter the T
+              itle..."
               value={formData.title}
+              ref={titleRef}
               //   setting the title
               // setFormData accepting the object
-              onChange={(e) => setFormData({title:e.target.value, content:formData.content})}
+              onChange={(e) =>
+                setFormData({
+                  title: e.target.value,
+                  content: formData.content,
+                })
+              }
             />
           </Row>
 
@@ -54,7 +67,9 @@ export default function Blog() {
               className="input content"
               placeholder="Content of the Blog..."
               value={formData.content}
-              onChange={(e) => setFormData({title:formData.title, content:e.target.value})}
+              onChange={(e) =>
+                setFormData({ title: formData.title, content: e.target.value })
+              }
             />
           </Row>
 
@@ -77,7 +92,6 @@ export default function Blog() {
             <button onClick={() => removeBlog(i)} className="btn remove">
               Delete
             </button>
-
           </div>
         </div>
       ))}
